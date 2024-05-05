8a822fff7fac8795f334e6a5dad811d9d5147959
John R Patek Sr <johnpatek2@gmail.com>
2024-05-05 06:56:55 +0000

    save point

--- pkg/rest/service.go
+++ pkg/rest/service.go
@@ -5,9 +5,11 @@ import (
 	"github.com/maxtek6/endpoints-go"
 )
 
+// Service is an HTTP router for all REST endpoints
 type Service struct {
-	router        *mux.Router
-	usersEndpoint *endpoints.Endpoint
+	router         *mux.Router
+	usersEndpoint  *endpoints.Endpoint
+	statusEndpoint *endpoints.Endpoint
 }
 
 func NewService() *Service {
